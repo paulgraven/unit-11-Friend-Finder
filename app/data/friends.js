bfa4e2914f5aca24b66a0d760c3a8c89fccd0a1e
Paul Graven <graven.paul@gmail.com>
2019-09-18 15:59:27 -0600

    added pictures for friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,7 +9,7 @@ var friends = [
   {
     name: "Tyler",
     photo:
-      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/32472011_10156421295291340_4118272324262690816_n.jpg?_nc_cat=100&_nc_oc=AQnXjEvv5b1c5mGswNbYz6LNKiiGuq3U4PQyfoyydLkBp670JeF4N1OGPDuRVUPKE8k&_nc_ht=scontent-ort2-1.xx&oh=de17698143c199fa7990eb79f370d68a&oe=5E02B741",
+      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/20842128_10212665751172370_1492433094772462646_n.jpg?_nc_cat=100&_nc_eui2=AeGcQ3Lom92gzsliwAylW7k2y_SW78dKJr9rxPEeH84cg1pEi3b8bruv5HcWOxbFIQcuNYiLNi1dcbRG7rGi3cHY0yuRgSQziM0bveStFCCp2g&_nc_oc=AQnqBVO6m5CDwLqVdFP-CjH1xbtiRr28CzhSGyWyx2_6wzH-88uo8cvdZz_mfFCwBaI&_nc_ht=scontent-ort2-1.xx&oh=16269b43c444cdfa796afb271b57359e&oe=5DFC0AC2",
     scores: [4, 4, 1, 3, 3, 5, 3, 2, 3, 4],
     best: false
   },
@@ -17,7 +17,7 @@ var friends = [
     name: "Leah",
     photo:
       "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/53415084_10214028994143671_5388976550978781184_n.jpg?_nc_cat=103&_nc_oc=AQmNeKjtDp2cJYrOreNTI1Y2i1NDbtu5c92mhAY1dM3lG-Q0FGjczzSVCiTpsjLUTx4&_nc_ht=scontent-ort2-1.xx&oh=f3323b796e037fc3f90dbb2b5f446156&oe=5DFA05CD",
-    scores: [5, 4, 4, 5, 5, 5, 4, 3, 2, 3],
+    scores: [4, 5, 2, 5, 4, 1, 4, 2, 3, 3],
     best: false
   },
   {
@@ -30,15 +30,22 @@ var friends = [
   {
     name: "Haleigh",
     photo:
-      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/53415084_10214028994143671_5388976550978781184_n.jpg?_nc_cat=103&_nc_oc=AQmNeKjtDp2cJYrOreNTI1Y2i1NDbtu5c92mhAY1dM3lG-Q0FGjczzSVCiTpsjLUTx4&_nc_ht=scontent-ort2-1.xx&oh=f3323b796e037fc3f90dbb2b5f446156&oe=5DFA05CD",
-    scores: [4, 5, 2, 5, 4, 1, 4, 2, 3, 3],
+      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/62213190_10214557816955919_5933526104294817792_n.jpg?_nc_cat=111&_nc_eui2=AeEIRHeAQME1XynnlNN1__SFrQxOuFuYnXMGoa66t4I4PzMJQCsTQ7DtXSyiwf8JNxzRAVrU8YCgYKMf00NYI6rRPg4i2SOpok00X_yFc-NXXg&_nc_oc=AQkn2n8-FK91p27eoG8n4io3AbpRhfLQJSzvKi9F7sKjQTyZx76oKGjcSZHrTGRTLLw&_nc_ht=scontent-ort2-1.xx&oh=b527a925e29432b20110862d9461567b&oe=5DF7298D",
+    scores: [5, 4, 4, 5, 5, 5, 4, 3, 2, 3],
     best: false
   },
   {
     name: "Ashley",
     photo:
-      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/53415084_10214028994143671_5388976550978781184_n.jpg?_nc_cat=103&_nc_oc=AQmNeKjtDp2cJYrOreNTI1Y2i1NDbtu5c92mhAY1dM3lG-Q0FGjczzSVCiTpsjLUTx4&_nc_ht=scontent-ort2-1.xx&oh=f3323b796e037fc3f90dbb2b5f446156&oe=5DFA05CD",
+      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/36490158_10212510415016872_9025726626964439040_n.jpg?_nc_cat=111&_nc_eui2=AeFjttgMIrl4Xfv0UmHQSmYaJe2X5H0Ncy9CuQR30PoVc8Pl7mADYFQYUMF958Of1klfZ8GZuBV3ka4Xz6RZ2Nc6h1AECp_M-5HLWkUnVgpY_w&_nc_oc=AQmbi94psKqXFYh1RkWcjeiLdeR5zfrrtDeX1bj8HThCz1sZtPnODYK4PVMuFKT48a8&_nc_ht=scontent-ort2-1.xx&oh=785db6c1a35d6d362a8c4115ee4b7cc5&oe=5DFF52F1",
     scores: [4, 1, 4, 1, 5, 2, 1, 1, 4, 4],
+    best: false
+  },
+  {
+    name: "Paul",
+    photo:
+      "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/11667360_10152895741716811_1446288929585404827_n.jpg?_nc_cat=100&_nc_eui2=AeH2eFE_Zja7kYI7FzM_eF0Z4uAWLPVOjydtG1UO5OZGa81V_AiO7JudYazrkS2i3VrQMhS-rSHebfkVXiD8wR_uM6UAPr8XeCKF0nsG5BWcng&_nc_oc=AQkUOLACR83SvkxGJfSML507-IuT7OJkugZSBW9Hq2Na1eQHqMgTkK2P8S6mZ1amhsY&_nc_ht=scontent-ort2-1.xx&oh=5d0356a51fea0d3a99d657567443a426&oe=5E34C80A",
+    scores: [4, 4, 5, 3, 1, 1, 5, 5, 5, 1],
     best: false
   }
 ];
