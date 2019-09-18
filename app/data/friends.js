69c9a10d810dddcbe383aba2cacab2ca0b6324f9
Paul Graven <graven.paul@gmail.com>
2019-09-18 15:18:02 -0600

    submit button now shows results page

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,14 +10,14 @@ var friends = [
     name: "Lindsey",
     photo:
       "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/32472011_10156421295291340_4118272324262690816_n.jpg?_nc_cat=100&_nc_oc=AQnXjEvv5b1c5mGswNbYz6LNKiiGuq3U4PQyfoyydLkBp670JeF4N1OGPDuRVUPKE8k&_nc_ht=scontent-ort2-1.xx&oh=de17698143c199fa7990eb79f370d68a&oe=5E02B741",
-    scores: [3, 1, 4, 5, 2, 5, 1, 4, 5, 1],
+    scores: [4, 2, 2, 4, 2, 2, 2, 2, 4, 3],
     best: false
   },
   {
     name: "Leah Bo-Beah",
     photo:
       "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/53415084_10214028994143671_5388976550978781184_n.jpg?_nc_cat=103&_nc_oc=AQmNeKjtDp2cJYrOreNTI1Y2i1NDbtu5c92mhAY1dM3lG-Q0FGjczzSVCiTpsjLUTx4&_nc_ht=scontent-ort2-1.xx&oh=f3323b796e037fc3f90dbb2b5f446156&oe=5DFA05CD",
-    scores: [4, 2, 2, 4, 2, 2, 2, 2, 4, 3],
+    scores: [3, 1, 4, 5, 2, 5, 1, 4, 5, 1],
     best: false
   }
 ];
